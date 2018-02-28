11aaf7d546dfb9bef04a4d40b08dcc1ed144eef5
pablofsf <ablocorrea@hotmail.com>
2018-02-28 12:43:51 +0100

    Needed for movement decision

---
+++ src/model/Movement.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Movement {
+	int probability;
+	int[] pos;
+	
+	public Movement(double probability, int row, int col, int head){
+		this.probability = (int) probability*100;
+		pos[0] = row;
+		pos[1] = col;
+		pos[2] = head;
+	}
+	
+	public int relativeProb(){
+		return probability;
+	}
+	
+	public int[] getPos(){
+		return pos;
+	}
+}
