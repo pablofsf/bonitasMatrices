e3bd7d30da4a11a34ffef09ce346b52f9add0e93
pablofsf <ablocorrea@hotmail.com>
2018-02-28 14:46:11 +0100

    Modified for our interface

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,6 +1,6 @@
 package control;
 
-import model.DummyLocalizer;
+import model.HMMPredictor;
 import view.RobotLocalizationViewer;
 
 public class Main {
@@ -15,7 +15,7 @@ public class Main {
 		 * generate you own localiser / estimator wrapper here to plug it into the 
 		 * graphics class.
 		 */
-		EstimatorInterface l = new DummyLocalizer( 1, 1, 1);
+		EstimatorInterface l = new HMMPredictor(8, 8);
 
 		RobotLocalizationViewer viewer = new RobotLocalizationViewer( l);
 
