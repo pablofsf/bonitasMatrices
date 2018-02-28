f425ea0f46fc72756b5038c2e4c411e5dc2a3ea7
pablofsf <ablocorrea@hotmail.com>
2018-02-28 13:04:40 +0100

    Needed to simulate sensor reading

---
+++ src/model/Reading.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Reading {
+	int probability;
+	int[] pos;
+	
+	public Reading(double probability, int row, int col){
+		this.probability = (int) probability*100;
+		pos = new int[2];
+		pos[0] = row;
+		pos[1] = col;
+	}
+	
+	public int relativeProb(){
+		return probability;
+	}
+	
+	public int[] getPos(){
+		return pos;
+	}
+}
