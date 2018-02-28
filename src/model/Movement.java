1759e8d7be568d9feeaa24686a3b4f0bd5f6db72
pablofsf <ablocorrea@hotmail.com>
2018-02-28 14:33:26 +0100

    Changed probability to double

--- src/model/Movement.java
+++ src/model/Movement.java
@@ -1,18 +1,18 @@
 package model;
 
 public class Movement {
-	int probability;
+	double probability;
 	int[] pos;
 	
 	public Movement(double probability, int row, int col, int head){
-		this.probability = (int) (probability*100);
+		this.probability =  (probability*100);
 		pos = new int[3];
 		pos[0] = row;
 		pos[1] = col;
 		pos[2] = head;
 	}
 	
-	public int relativeProb(){
+	public double relativeProb(){
 		return probability;
 	}
 	
--- src/model/Reading.java
+++ src/model/Reading.java
@@ -1,17 +1,17 @@
 package model;
 
 public class Reading {
-	int probability;
+	double probability;
 	int[] pos;
 	
 	public Reading(double probability, int row, int col){
-		this.probability = (int) (probability*100);
+		this.probability = (probability*100);
 		pos = new int[2];
 		pos[0] = row;
 		pos[1] = col;
 	}
 	
-	public int relativeProb(){
+	public double relativeProb(){
 		return probability;
 	}
 	
